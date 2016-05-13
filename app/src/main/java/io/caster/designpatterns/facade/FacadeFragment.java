11fc9f392f8629b5d1f116b47ed7996a97dfcc57
Donn Felker <donn@donnfelker.com>
2016-05-13 17:50:51 -0400

    Adding facade fragment

---
+++ app/src/main/java/io/caster/designpatterns/facade/FacadeFragment.java
@@ -0,0 +1,30 @@
+package io.caster.designpatterns.facade;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import io.caster.designpatterns.R;
+
+public class FacadeFragment extends Fragment {
+
+    private TextView value;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.facade_fragment, container, false);
+        value = (TextView) v.findViewById(R.id.facade_value);
+        return v;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+    }
+}
